8b5d01c0f4c7f12b0c63d2eaaea581d209599631
victor satoh <vsatoh@gmail.com>
2023-09-22 16:59:47 -0300

    inicio do lab08a

---
+++ lab08/lab08a/lab08a.s
@@ -0,0 +1,12 @@
+.section .text
+.globl _start
+
+open:
+    la a0, input_file    # address for the file path
+    li a1, 0             # flags (0: rdonly, 1: wronly, 2: rdwr)
+    li a2, 0             # mode
+    li a7, 1024          # syscall open 
+    ecall
+
+input_file: .asciz "image.pgm"
+
