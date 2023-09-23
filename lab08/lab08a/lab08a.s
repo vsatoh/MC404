316bced429c57b9378f834df716b6f3c399be8ed
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-09-22 22:33:59 -0300

    le arquivo

--- lab08/lab08a/lab08a.s
+++ lab08/lab08a/lab08a.s
@@ -7,6 +7,29 @@ open:
     li a2, 0             # mode
     li a7, 1024          # syscall open 
     ecall
+    ret
 
 input_file: .asciz "image.pgm"
 
+read:
+    la a1, input_address #  buffer to write the data
+    li a2, 262144  # size (reads only 1 byte)
+    li a7, 63 # syscall read (63)
+    ecall
+    ret
+
+write:
+    li a0, 1          
+    la a1, input_address       
+    li a2, 262144       
+    li a7, 64          
+    ecall    
+    ret
+
+_start:
+    jal ra, open
+    jal ra, read
+    jal ra, write
+
+.section .data
+input_address: .skip 262144
