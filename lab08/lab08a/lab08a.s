f608d48395706c89e955a355fd5977ab0b9a2394
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-09-26 14:40:12 -0300

    lab8a, pixel error 1

--- lab08/lab08a/lab08a.s
+++ lab08/lab08a/lab08a.s
@@ -13,7 +13,7 @@ input_file: .asciz "image.pgm"
 
 read:
     la a1, input_address #  buffer to write the data
-    li a2, 262144  # size (reads only 1 byte)
+    li a2, 262159 # size (reads only 1 byte)
     li a7, 63 # syscall read (63)
     ecall
     ret
@@ -21,15 +21,108 @@ read:
 write:
     li a0, 1          
     la a1, input_address       
-    li a2, 262144       
+    li a2, 262159       
     li a7, 64          
     ecall    
+    ret
+
+read_size:
+    li t0, ' '
+    li t1, 10
+    li s0, 0
+    for_rdsz:
+        lbu a4, 0(a3)
+        addi a3, a3, 1
+        beq a4, t0, fim_rdsz
+        mul s0, s0, t1
+        addi a4, a4, -48
+        add s0, s0, a4
+        j for_rdsz
+    fim_rdsz:    
+    ret
+
+read_size2:
+    li t0, '\n'
+    li t1, 10
+    li s1, 0
+    for_rdsz2:
+        lbu a4, 0(a3)
+        addi a3, a3, 1
+        beq a4, t0, fim_rdsz2
+        mul s1, s1, t1
+        addi a4, a4, -48
+        add s1, s1, a4
+        j for_rdsz2
+    fim_rdsz2:  
+    ret
+
+setPixel:
+    mv a0, t0
+    mv a1, t1
+    mv a2, t2
+    li a7, 2200 # syscall setPixel (2200)
+    ecall
+    ret
+
+setCanvasSize:
+    mv a0, s0
+    mv a1, s1
+    li a7, 2201
+    ecall
+    ret
+
+
+printImg:
+    mv s3, ra #armazena o valor de ra 
+    li t0, 0
+    for1:
+        bge t0, s0, cont1
+        li t1, 0
+        for2:
+            bge t1, s1, cont2
+            lbu t2, 0(a3) 
+            mv t6, t2
+
+            slli t2, t2, 8
+            add t2,t2, t6 
+            slli t2, t2, 8
+            add t2,t2, t6
+            slli t2, t2, 8
+            addi t2, t2, 255
+
+            jal ra, setPixel
+            addi t1, t1, 1
+            addi a3, a3, 1
+            j for2
+        cont2:
+        addi t0, t0, 1
+        j for1
+    cont1:
+    mv ra, s3
     ret
 
 _start:
     jal ra, open
     jal ra, read
-    jal ra, write
+    #jal ra, write ate aqui funciona
+
+    la a3, input_address
+    addi a3, a3, 3
+    #s0 widht s1 height
+    jal ra, read_size
+
+    jal ra, read_size2
+
+
+    jal ra, setCanvasSize
+
+
+    addi a3, a3, 4
+    jal ra, printImg
+
+    li a0, 0
+    li a7, 93
+    ecall
 
 .section .data
-input_address: .skip 262144
+input_address: .skip 262159
