35633e72be93bb4d6dad879e89ec7d78dd97f6f7
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-10-14 22:42:58 -0300

    int_atoi consertada - n testado

--- lab10/lab10a/lab10a.s
+++ lab10/lab10a/lab10a.s
@@ -21,18 +21,27 @@ void_puts:
     #Changes \0 for \n
     #string comes in a0
     #needs to write the string
+    la a1, output_address
     li t0, '\0'
     lb t1, 0(a0)
     li t2, 0
     for_void_puts:
         addi t2, t2, 1
+        sb t1, 0(a1)
         beq t0, t1, fim_void_puts
         addi a0, a0, 1
+        addi a1, a1, 1
         lb t1, 0(a0)
         j for_void_puts
     fim_void_puts:
     li t0, '\n'
     sb t0, 0(a0)
+
+    li a0, 1          
+    la a1, output_address       
+    li a2, 20           
+    li a7, 64          
+    ecall    
     ret
 
 str_gets:
@@ -55,44 +64,61 @@ str_gets:
 
 int_atoi:
     #(a0 char * str)
-    la a0, input_address
+    #ignorar espacos antes e quaisquer tipo de caracter dps
+    #return in a0 the value of char 
+    for1:
+        lb a3, 0(a0)
+        li t0, ' '
+        bne a3, t0, fim1
+        addi a0, a0, 1
+        j for1
+    fim1:
     lb a3, 0(a0)
     li a5, 0 #num de entrada
     li a4, '-'
     beq a3, a4, num_neg
     li a4, '\0'
-    for1:
-        beq a3, a4, fim
+    for2:
+        beq a3, a4, fim2
+        li a4, '/'
+        blt a4, a3, brk2
+        li a4, '9'
+        bge a3, a4, brk2
         li a4, 10
         mul a5, a5, a4
         addi a3, a3, -48
         add a5, a3, a5
+        brk2:
         addi a0, a0, 1
         lb a3, 0(a0)
         li a4, '\0'
-        j for1
+        j for2
 
     num_neg:
         addi a0, a0, 1
         li a4, '\0'
         lb a3, 0(a0)
-        for2:
-            beq a3, a4, fim2
+        for3:
+            beq a3, a4, fim3
+            li a4, '/'
+            blt a4, a3, brk2
+            li a4, '9'
+            bge a3, a4, brk2
             li a4, 10
             mul a5, a5, a4
             addi a3, a3, -48
             add a5, a3, a5
+            brk3:
             addi a0, a0, 1
             lb a3, 0(a0)
             li a4, '\0'
-            j for2
-        fim2:
+            j for3
+        fim3:
         li a4, -1
         mul a5, a5, a4
-    fim:
+    fim2:
     mv a0, a5
     ret
-
 
 char_itoa:
     #(a0 int value,a1 char * str,a2 int base )
@@ -135,14 +161,16 @@ char_itoa:
         addi t0, t0, 1
         j for_char_itoa2
     fim_for_char_itoa2:
+    # addi a1, a1, 1
+    # li a4, '\n'
+    # sb a4, 0(a1)
     ret
 
 _start:
-    li a0, 1521
-    li a2, 16
-    la a1, output_address
-    jal ra, char_itoa
-    jal ra, write
+    la a0, string
+    jal ra, int_atoi
 .section .data
 input_address: .skip 7
 output_address: .skip 7
+
+string: .asciz "1010\0"
