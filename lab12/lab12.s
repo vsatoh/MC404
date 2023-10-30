255ec277aa93963425ffe92033ad51873c901af6
Victor satoh <vsatoh@gmail.com>
2023-10-30 12:11:15 -0300

    hex neg consertar

--- lab12/lab12.s
+++ lab12/lab12.s
@@ -3,12 +3,19 @@
 
 read:
     #a0 base, a1 string 
+    li t2, 0
     for_read:        
         la a0, base
         addi a0, a0, 2
 
         li t0, 1
         sb t0, 0(a0)
+
+        brk_read: #n avancar enquanto n terminar a leitura
+            lb t0, 0(a0)
+            li t1, 0
+        bne t0, t1, brk_read
+
 
         addi a0, a0, 1
         lb t0, 0(a0)
@@ -17,6 +24,7 @@ read:
         addi a1, a1, 1
 
         li t1, '\n'
+        addi t2, t2, 1
         beq t1, t0, fim_for_read
         j for_read
     fim_for_read: 
@@ -30,8 +38,14 @@ write:
 
         #carrega e imprime
         sb t0, 1(a0)
+
         li t1, 1
         sb t1, 0(a0)
+
+        brk_write: #n avancar enquanto n terminar a leitura
+            lb t2, 0(a0)
+            li t1, 0
+        bne t2, t1, brk_write
 
         #condicao de parada
         addi a1, a1, 1
@@ -42,7 +56,7 @@ write:
     ret
 
 ret_string:
-    la a1, input_address
+    la a1, output_address
     li t3, 0
     for_ret_string1:        
         la a0, base
@@ -50,6 +64,12 @@ ret_string:
 
         li t0, 1
         sb t0, 0(a0)
+
+        brk_ret_string1: #n avancar enquanto n terminar a leitura
+            lb t0, 0(a0)
+            li t1, 0
+        bne t0, t1, brk_ret_string1
+
 
         addi a0, a0, 1
         lb t0, 0(a0)
@@ -64,6 +84,7 @@ ret_string:
     fim_ret_string1: 
 
     sub a1, a1, t3
+    #la a1, output_address
     
     for_ret_string2:
         la a0, base
@@ -71,8 +92,14 @@ ret_string:
 
         #carrega e imprime
         sb t0, 1(a0)
+
         li t1, 1
         sb t1, 0(a0)
+
+        brk_ret_string2: #n avancar enquanto n terminar a leitura
+            lb t0, 0(a0)
+            li t1, 0
+        bne t0, t1, brk_ret_string2
 
         #condicao de parada
         addi a1, a1, 1
@@ -87,12 +114,13 @@ reverse_string:
     addi sp, sp, -4
     sw ra, 0(sp)
 
+    la a1, output_address
     jal ra, read
 
     addi sp, sp, 4
     lw ra, 0(sp)
 
-    la a1, input_address
+    la a1, output_address
 
     li t0, '\n'
     addi sp, sp, -4
@@ -117,8 +145,15 @@ reverse_string:
 
         #carrega e imprime
         sb t0, 1(a0)
+
         li t1, 1
         sb t1, 0(a0)
+
+
+        brk_reverse_string2: #n avancar enquanto n terminar a leitura
+            lb t2, 0(a0)
+            li t1, 0
+        bne t2, t1, brk_reverse_string2
 
         #condicao de parada
         li t1, '\n'
@@ -128,11 +163,24 @@ reverse_string:
     ret
 
 hex_representation:
+
+    #le uma entrada nova
+    addi sp, sp, -4
+    sw ra, 0(sp)  
+
+    la a1, output_address
+    jal ra, read
+
+    lw ra, 0(sp)
+    addi sp, sp, 4
+
     #converte para inteiro (ret em a0)
     addi sp, sp, -4
     sw ra, 0(sp)  
 
+    la a0, output_address
     jal ra, atoi
+    mv t0, a0
 
     lw ra, 0(sp)
     addi sp, sp, 4
@@ -142,6 +190,8 @@ hex_representation:
     sw ra, 0(sp) 
 
     li a2, 16
+    mv a0, t0
+    la a1, output_address
     jal ra, itoa
 
     lw ra, 0(sp)
@@ -151,6 +201,7 @@ hex_representation:
     addi sp, sp, -4
     sw ra, 0(sp) 
 
+    la a1, output_address
     jal ra, write
 
     lw ra, 0(sp)
@@ -162,9 +213,12 @@ calculator:
 
 atoi:
     #(a0 char * str)
-    #ignorar espacos antes e quaisquer tipo de caracter dps
     #return in a0 the value of char 
     lb a3, 0(a0)
+    li a5, 0
+
+    li a4, '-'
+    beq a3, a4, num_neg
     for2:
         li a4, '\n'
         beq a3, a4, fim2
@@ -176,12 +230,41 @@ atoi:
         addi a0, a0, 1
         lb a3, 0(a0)
         j for2
+
+    num_neg:
+        addi a0, a0, 1
+        li a4, 0
+        lb a3, 0(a0)
+        for3:
+            li a4, '\n'
+            beq a3, a4, fim3
+            li a4, 10
+            mul a5, a5, a4
+            addi a3, a3, -48
+            add a5, a3, a5
+            brk3:
+            addi a0, a0, 1
+            lb a3, 0(a0)
+            j for3
+        fim3:
+        li a4, -1
+        mul a5, a5, a4
+
     fim2:
     mv a0, a5
     ret
 
 itoa:
+    #a0 num, a2 base
     li t1, 0
+    bge a0, t1, for_char_itoa
+    li t1, 4294967295
+    li t1, -1
+    mul a0, a0, t1
+    addi a0, a0, -1
+    xor a0, a0, t1
+    li t1, 0
+
     for_char_itoa:
         addi t1, t1, 1 #numero de digitos
         rem t0, a0, a2 #resto da divisao
@@ -226,13 +309,24 @@ _start:
     jal ra, atoi
 
     #switch case
-    mv t0, a0 #entrada inicial
-
-    jal ra, ret_string
+    #jal ra, ret_string
+    #jal ra, reverse_string
+    jal ra, hex_representation
 
 .section .data
 input_address: .skip 100
 output_address: .skip 100
+output_address2: .skip 100
 
 
 .set base, 0xFFFF0100
+
+#checklist
+#ret_string ok
+#reverse_string ok
+#hexa_base
+#calculator
+#soma
+#sub
+#div
+#mul
